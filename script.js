7eba1a68c2f7992d8f549ca36994f815fb437809
datakvitsiani <datuka.colt@gmail.com>
2022-11-06 13:36:37 +0400

    finished

---
+++ script.js
@@ -0,0 +1,92 @@
+'use strict';
+
+// დავ. 1 
+// function sum(...numbers) {
+//  let sum = 0;
+//  for (let item of numbers) {
+//     if(item > 0) {
+//     sum += item;
+//      }
+//     }
+//     return sum;
+// }
+// let result = sum(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+// console.log(result);
+
+// დავ. 2
+
+// function sum(...numbers) {
+//     let numberSum = 0;
+//     numbers = [10, 50, 6, 7, 8, 11, 6, 3, 9]
+//     for (let item of numbers) {
+//     numberSum += item;
+//     }
+//     return numberSum;
+//     }
+//     let result = sum(10, 50, 6, 7, 8, 11, 6, 3, 9)
+//     console.log(result);
+
+// დავ. 3
+
+// let user = {
+//     firstname: 'giorgi',
+//     lastname: 'saakadze',
+//     age: 32,
+//     isloggedin: true
+//   }
+
+//  let printName = `${user.firstname} ${user.lastname} ${user.isloggedin}`;
+//   console.log(printName);
+
+// დავ. 4 
+
+// function getMaxValue(numbers) {
+//     let maxValue = 0;
+//     for (let item of numbers) {
+//         if (item > maxValue) {
+//             maxValue = item;
+//         }
+//     }
+//     return maxValue;
+// }
+
+// let resultMaxValue = getMaxValue([12, 13, 80, 45, 130, 500,])
+// console.log(resultMaxValue);
+
+// დავ. 5
+
+// let array = [1,2,4,10,34,5,7,87]
+
+// for (let item of array) {
+//     if (item == 10 ) {
+//         break
+//     }
+//     console.log(item);
+// }
+
+// დავ. 6
+
+// let numbers =[1,2,3,4,5,6,7,8,9,10]
+
+// for (let item of numbers) {
+//     if (item == 5 ) {
+//         break
+//     }
+//     console.log(item);
+// }
+
+// დავ. 7
+
+// let array = ['html', 'css', 'python', 'javascript', 'bootstrap'];
+
+// array.forEach(function(y) {
+//     if(y.length > 4 && y.includes('av')) {
+//         console.log(y);
+//     }
+// })
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i].length > 4 && array[i].includes('av') ) {
+//         console.log(array[i]);
+//     }
+// }
